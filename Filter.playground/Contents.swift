44f6fd9a3df9909a0fc4bc055b36491a4944febb
Chu <cnpanic@naver.com>
2018-01-30 15:40:54 +0900

    filter

--- Filter.playground/Contents.swift
+++ Filter.playground/Contents.swift
@@ -1,5 +1,15 @@
 //: Playground - noun: a place where people can play
-
+//Filter함수는 반복문과 조건문의 조합
 import UIKit
 
-var str = "Hello, playground"
+
+let transactions = [560.0, 345.5, 492.2, 1912.4, 20000.1, 12.4]
+var bigTransitions : [Double] = []
+
+
+for transaction in transactions {
+    if transaction >= 500{
+        bigTransitions += [transaction]
+    }
+}
+

cd99f2af97ab0e21139ca164a725c25faac8dfdf
Chu <cnpanic@naver.com>
2018-01-30 15:45:04 +0900

    filter함수 -2

--- Filter.playground/Contents.swift
+++ Filter.playground/Contents.swift
@@ -13,3 +13,13 @@ for transaction in transactions {
     }
 }
 
+// 이건 단순히 for문을 이용해 500이 넘는 값을 배열에 넣고 있다.
+
+//filter함수를 이용하면 이렇게 된다.
+
+let bigFilterTransactions = transactions.filter { $0 >= 500 }
+
+// filter함수는 배열 요소를 매개 변수로 받고, Bool형으로 반환하는 함수를 전달받는다.
+// 각각의 요소가 조건을 충족하는지 그렇지 않은지를 검사해 반환한다.
+
+
